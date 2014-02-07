82aecf23003b1d46a64882cfa0ebd7d6735adcc4
Chad Whitacre <chad@zetaweb.com>
2014-02-07 17:39:06 -0500

    Rough in CSS from www.gittip.com and SCSS locally

--- configure-aspen.py
+++ configure-aspen.py
@@ -1,6 +1,9 @@
+import commands
 import os
+from collections import namedtuple
 from os.path import basename, dirname, join, realpath, isdir
 
+import aspen.execution
 from nav import NavItem
 
 
@@ -47,3 +50,25 @@ if not isdir(website.repo_root):
     os.system('mkdir {}'.format(website.repo_root))
     for repo in repos:
         os.system('cd {} && git clone {}'.format(website.repo_root, repo))
+
+
+# Set website.versions.
+# =====================
+
+def get_www_gittip_com_version():
+    version_file = join(website.repo_root, 'www.gittip.com', 'www', 'version.txt')
+    aspen.execution.extras.add(version_file)
+    version = open(version_file).read().strip()
+    if version.endswith('-dev'):
+        version = version[:-len('-dev')]
+    return version
+
+website.versions = namedtuple('Versions', 'www_gittip_com gttp_co Gittip_Everywhere ours'.split())
+website.versions.ours = commands.getoutput('git rev-parse HEAD')
+website.versions.www_gittip_com = get_www_gittip_com_version()
+
+
+# Set random thing that we're not really using.
+# =============================================
+
+website.compress_assets = True
