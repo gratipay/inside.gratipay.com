gratipay/inside.gratipay.com

commit 17f9408769fbe5df05a6ba510579ee01b7b8c0c4

Simplify Makefile around env files
We don't need to copy defaults.local.env to local.env if we're also
layering them.

Makefile
@@ -20,11 +20,6 @@ clean:
 	rm -rf env *.egg *.egg-info
 	find . -name \*.pyc -delete
 
-local.env:
-	echo "Creating a local.env file ..."
-	echo
-	cp default.local.env local.env
-
 run: env local.env
-	./$(env_bin)/honcho -e default.local.env,local.env run ./env/bin/aspen \
+	./$(env_bin)/honcho -e defaults.env,local.env run ./env/bin/aspen \
 		--network_address :8536 --www_root=./www/ --project_root=./

commit 3e38a1727901318e53ceed3165ababe54b114e1a

Fix #182 - `make run` fails
Leftover from 17f9408769fbe

Makefile
@@ -20,6 +20,6 @@ clean:
 	rm -rf env *.egg *.egg-info
 	find . -name \*.pyc -delete
 
-run: env local.env
+run: env
 	./$(env_bin)/honcho -e defaults.env,local.env run ./env/bin/aspen \
 		--network_address :8536 --www_root=./www/ --project_root=./