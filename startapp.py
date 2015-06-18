8449bd2d1ae72a91900f02cf90a65f117345ceb2
anatoly techtonik <techtonik@gmail.com>
2015-06-18 15:35:59 +0300

    Show the host:port for application access
    I don't use localhost, because I am running this on remote node

--- startapp.py
+++ startapp.py
@@ -12,4 +12,6 @@ application = Website([])
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
+    # [ ] FIXME with real host:port
+    print("* Running development server on http://0.0.0.0:8000")
     make_server('', 8000, application).serve_forever()
