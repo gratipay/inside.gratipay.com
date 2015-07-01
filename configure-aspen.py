ffbae67c97809af334332b9bcba4ec8eddc84e81
anatoly techtonik <techtonik@gmail.com>
2015-07-01 19:53:04 +0300

    Get rid of request.fs

--- configure-aspen.py
+++ configure-aspen.py
@@ -26,11 +26,11 @@ website.renderer_default = "markdown"
 def add_nav_to_website(website):
     website.nav = NavItem(website, None, website.www_root, '')
 
-def add_nav_current_to_context(request, website):
-    fs = request.fs
-    if basename(request.fs) in website.indices:
-        fs = dirname(fs)
-    request.context['nav_current'] = website.nav.by_fs.get(fs, website.nav)
+def add_nav_current_to_context(request, website, dispatch_result):
+    path = dispatch_result.match
+    if basename(path) in website.indices:
+        path = dirname(path)
+    request.context['nav_current'] = website.nav.by_fs.get(path, website.nav)
 
 def add_nav_next_to_context(request, website):
     nav_current = request.context['nav_current']
